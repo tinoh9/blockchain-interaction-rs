a7ae3b92a074117a382fb3f01a22ca15e8f01d16
tinoh9 <bachhuynh959@gmail.com>
2022-11-28 13:14:53 +0700

    Update test.rs

--- test.rs
+++ test.rs
@@ -10,3 +10,26 @@ pair.1;
 let couple: (u64, char) = (90, 'a');
 
 let smallest = std::cmp::min(2,9);
+
+fn one_number_1() -> i32 {
+    5
+}
+
+fn one_number_2() -> i32 {
+    return 5;
+}
+
+fn dice_roll_1() -> i32 {
+    if so_lucky {
+        6
+    } else {
+        1
+    }
+}
+
+fn dice_roll_2() -> i32 {
+    match stat {
+        true => 6
+        false => 2
+    }
+}
