ff2c28eea2caf5fb1c4365242591a9a888d35939
tinoh9 <bachhuynh959@gmail.com>
2023-02-04 22:57:23 +0700

    Update test.rs

--- test.rs
+++ test.rs
@@ -1444,7 +1444,7 @@ impl Shape {
     fn area(&self) -> f64 {
         match *self {
             Shape::Rectangle {width, height} => width * height,
-            Shape::Circle {radius} => 3.14159265358979 * radius * radius,
+            Shape::Circle {radius} => 3.14 * radius * radius,
             Shape::Triangle {base, height} => 0.5 * base * height,
         }
     }
