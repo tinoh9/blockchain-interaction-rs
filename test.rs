f17fe10547ec540dbb46ab255c95e7a88b9ca311
tinoh9 <bachhuynh959@gmail.com>
2023-02-11 23:19:35 +0700

    Update test.rs

--- test.rs
+++ test.rs
@@ -1569,3 +1569,30 @@ fn main() {
     user.set_password("even-more-secret");
 }
 
+//// Test: count the letters and numbers of an input string within a module which has several sub-modules
+mod text_processing {
+
+    pub mod letters {
+        pub fn count_letters(text: &str) -> usize {
+            text.chars().filter(|ref c| c.is_alphabetic()).count()
+        }
+    }
+
+    pub mod numbers {
+        pub fn count_numbers(text: &str) -> usize {
+            text.chars().filter(|ref c| c.is_numeric()).count()
+        }
+    }
+}
+
+fn count_letters_and_numbers(text: &str) -> (usize, usize) {
+    let number_of_letters = text_processing::letters::count_letters(text);
+    let number_of_numbers = text_processing::numbers::count_numbers(text);
+    (number_of_letters, number_of_numbers)
+}
+
+fn main() {
+    assert_eq!(count_letters_and_numbers("221B Baker Street"), (12, 3));
+    assert_eq!(count_letters_and_numbers("711 Maple Street"), (11, 3));
+    assert_eq!(count_letters_and_numbers("4 Privet Drive"), (11, 1));
+}
